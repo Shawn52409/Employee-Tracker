82890df01e1603c5a93c403e1a2d68c29ceff6ce
Shawn Miller <shawnsmm@gmail.com>
2021-10-16 19:55:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Employee Tracker
+Created by Shawn Miller
+
+# Description
+This app was created for a 
+
+# Installation Instructions
+
+
+# Technologies Used
+- Node.js
+- npm Inquirer
+- npm fs
+- JavaScript
+- Express.js
+- JSON
+
+# Appearance
+![Screenshot of README and command line](./img/screenshot.jpg)
+
+# Known Bugs
+No known bugs.
+
+# Contact info
+shawnsmm@gmail.com
+
+github.com/Shawn52409
+
+www.linkedin.com/in/shawn-miller-b44a36217
---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE registrar_db;
+
+-- Department table
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+-- Roles table
+CREATE TABLE roles (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id),
+  REFERENCES department(id),
+  ON DELETE SET NULL
+);
+
+-- Employee Table
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  FOREIGN KEY (role_id),
+  REFERENCES role(id),
+  PRIMARY KEY (id),
+  ON DELETE SET NULL
+);
