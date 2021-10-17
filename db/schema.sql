189f1dd771d29f2adc747b0f2848395b60a7f75a
Shawn Miller <shawnsmm@gmail.com>
2021-10-16 20:04:46 -0400

    update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
-USE registrar_db;
+USE employee_db;
 
 -- Department table
 CREATE TABLE department (
@@ -10,14 +10,13 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 -- Roles table
-CREATE TABLE roles (
+CREATE TABLE role (
   id INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (department_id),
-  REFERENCES department(id),
+  FOREIGN KEY (department_id) REFERENCES department(id),  
   ON DELETE SET NULL
 );
 
@@ -28,8 +27,8 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NOT NULL,
-  FOREIGN KEY (role_id),
-  REFERENCES role(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id),
   PRIMARY KEY (id),
   ON DELETE SET NULL
 );
